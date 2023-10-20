8b9298aafc65ced23a976e88c86d548831ecc08b
Kristina Pianykh <kristinavrnrus@gmail.com>
2023-10-20 18:26:25 +0200

    chore: fail deployment on non-0 exit code for docker push

--- terraform_deploy.sh
+++ terraform_deploy.sh
@@ -3,17 +3,39 @@
 export $(cat env | xargs)
 ACR_NAME="${PROJECT_NAME}acr"
 IMAGE_NAME="${PROJECT_NAME}-image"
+export TF_VAR_image_name="${IMAGE_NAME}"
 
-terraform -chdir=src/terraform init \
- -backend-config="resource_group_name=$RESOURCE_GROUP" \
- -backend-config="storage_account_name=$STORAGE_ACCOUNT_NAME" \
- -backend-config="container_name=$CONTAINER_NAME" \
- -backend-config="key=terraform.tfstate"
+# terraform authentication with Azure
+export TF_VAR_subscription_id="${ARM_SUBSCRIPTION_ID}"
+export TF_VAR_client_id="${ARM_CLIENT_ID}"
+export TF_VAR_client_secret="${ARM_CLIENT_SECRET}"
+export TF_VAR_tenant_id="${ARM_TENANT_ID}"
 
-terraform -chdir=src/terraform plan -out=plan.out
-terraform -chdir=src/terraform apply plan.out
+terraform_deploy() {
+    terraform -chdir=src/terraform init -upgrade \
+    -backend-config="resource_group_name=$RESOURCE_GROUP" \
+    -backend-config="storage_account_name=$STORAGE_ACCOUNT_NAME" \
+    -backend-config="container_name=$CONTAINER_NAME" \
+    -backend-config="key=terraform.tfstate"
 
-docker image build -t $IMAGE_NAME .
-docker tag $IMAGE_NAME streamlittoyappacr.azurecr.io/$IMAGE_NAME
-az acr login --name $ACR_NAME
-docker push streamlittoyappacr.azurecr.io/$IMAGE_NAME
+    terraform -chdir=src/terraform plan -out=plan.out
+    terraform -chdir=src/terraform apply plan.out
+}
+
+push_image() {
+    docker image build -t $IMAGE_NAME .
+    docker tag $IMAGE_NAME streamlittoyappacr.azurecr.io/${IMAGE_NAME}
+    az acr login --name $ACR_NAME
+    docker push streamlittoyappacr.azurecr.io/${IMAGE_NAME}
+}
+
+push_image
+
+if [ $? -eq 0 ]; then
+    echo "Docker images pushed successfully. Proceeding to terraform deploy..."
+    terraform_deploy
+    echo "Terraform deployment completed."
+else
+    echo "Docker image push failed. Exiting..."
+    exit 1
+fi
