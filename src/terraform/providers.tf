e4af4651089f2e0a0d7dc1f902ea7d289faa94dc
Kristina Pianykh <kristinavrnrus@gmail.com>
2023-10-20 20:21:22 +0200

    fix: force destroy on resource group

--- src/terraform/providers.tf
+++ src/terraform/providers.tf
@@ -19,7 +19,11 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
   client_id       = var.client_id
