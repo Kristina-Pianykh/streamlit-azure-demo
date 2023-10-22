53da187ed24ba14ac541dc0d83b1b80c72640a16
Kristina Pianykh <kristinavrnrus@gmail.com>
2023-10-22 04:47:50 +0200

    chore: create missing resources on bootstrap

--- terraform_backend.sh
+++ terraform_backend.sh
@@ -12,11 +12,22 @@ fi
 
 export $(cat $path | xargs)
 
-# Create resource group
-az group create --name $RESOURCE_GROUP_NAME --location $LOCATION
+# check if resource group exists and create if not
+if [ $(az group exists --name $RESOURCE_GROUP) = false ]; then
+    echo "Resource group $RESOURCE_GROUP does not exist. Creating..."
+    az group create --name $RESOURCE_GROUP --location $LOCATION
+    GROUP_ID=$(az group show --name $RESOURCE_GROUP --query id --output tsv)
+    echo "Resource group $RESOURCE_GROUP created."
+else
+    echo "Resource group $RESOURCE_GROUP already exists."
+fi
 
-# Create storage account
-az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
-
-# Create blob container
-az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME
+# check if storage account exists and create if not
+if [ $(az storage container exists --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME | jq -r '.exists') = "false" ]; then
+    echo "Storage container $CONTAINER_NAME does not exist. Creating..."
+    az storage account create --resource-group $RESOURCE_GROUP --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
+    az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME
+    echo "Storage container $CONTAINER_NAME created."
+else
+    echo "Storage container $CONTAINER_NAME already exists."
+fi
