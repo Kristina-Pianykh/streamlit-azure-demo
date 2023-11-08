265442027bfd05ad8c5e789212751bcc09ccb92d
Kristina Pianykh <kristinavrnrus@gmail.com>
2023-11-08 14:27:16 +0100

    fix: remove client_id from tf provider

--- src/terraform/providers.tf
+++ src/terraform/providers.tf
@@ -32,7 +32,6 @@ provider "azurerm" {
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
   client_id       = var.client_id
-  client_secret   = var.client_secret
 }
 
 # Generate a random integer to create a globally unique name
